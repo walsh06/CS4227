c2d12f67d9936e577a42be337ddd9356762cf127
HallCat <cathal99h@gmail.com>
2014-10-28 12:23:49 +0000

    changes to main

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,7 @@
 #include <gtest/gtest.h>
 
-
-    #include <EnemyItemDecorator.h>
-    #include <Enemy.h>
-    #include <BasicEnemy.h>
-
-    #include <SpeedItem.h>
-
 int main(int argc, char** argv)
 {
     testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
-
-
-
 }
