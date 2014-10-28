b3b23bc80f3dfd363cc073d9c25cdfbeb2fa2029
HallCat <cathal99h@gmail.com>
2014-10-28 13:34:36 +0000

    Adding in @walsh06's composite changes

---
+++ include/EnemyWave.h
@@ -0,0 +1,29 @@
+#ifndef ENEMYWAVE_H
+#define ENEMYWAVE_H
+
+#include "EnemyInterface.h"
+#include <vector>
+
+class EnemyWave : public EnemyInterface
+{
+    public:
+        EnemyWave(int waveSize);
+        virtual ~EnemyWave();
+
+        void setHealth(int health);
+        void setPower(int power);
+
+        void addEnemy(EnemyInterface* enemy);
+        void removeEnemy(EnemyInterface* enemy);
+        EnemyInterface* getEnemy(int enemyIndex);
+
+        void update();
+
+    protected:
+    private:
+
+        std::vector<EnemyInterface *> wave;
+        int waveSize;
+};
+
+#endif // ENEMYWAVE_H
