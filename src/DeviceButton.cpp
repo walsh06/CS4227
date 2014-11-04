8e3bce383daae2a295553b264d5702358ad71e5a
Killian Moore <killian.moore1@yahoo.ie>
2014-11-04 14:12:03 +0000

    Adding the renamed DeviceButton

---
+++ src/DeviceButton.cpp
@@ -0,0 +1,19 @@
+#include "DeviceButton.h"
+
+//The invoker which takes a Concrete Command as a parameter.
+DeviceButton::DeviceButton(Command* command)
+{
+    this->aCommand = command;
+}
+
+//Destructor.
+DeviceButton::~DeviceButton()
+{
+    delete aCommand;
+}
+
+//Called when a button is pressed.
+void DeviceButton::press()
+{
+    aCommand->execute();
+}
